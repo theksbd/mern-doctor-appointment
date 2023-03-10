ff31bea37021648e912e966d75068d68ffbfedae
Do Thien Hoang <theksbd@gmail.com>
2023-03-10 21:12:30 +0700

    update changes

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
-const app = express();
-require("dotenv").config();
 const dbConfig = require("./config/dbConfig");
-app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/doctorsRoute");
 
+const app = express();
+
+app.use(express.json());
 app.use(cors());
+
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorRoute);
 
 app.get("/", (req, res) => {
-  res.send("Hello World from server.js!");
+  res.json({ message: "Hello World from server.js!" });
 });
 
 const port = process.env.PORT || 5000;
